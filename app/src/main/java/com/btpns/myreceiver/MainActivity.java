73d67b00a593b926374c4b4272dace08f6e1b1aa
ADMIN <v-Dwi.Prayogo@btpnsyariah.com>
2020-05-20 07:11:49 +0700

    broadcast for data readiness prss

--- app/src/main/java/com/btpns/myreceiver/MainActivity.java
+++ app/src/main/java/com/btpns/myreceiver/MainActivity.java
@@ -2,13 +2,33 @@ package com.btpns.myreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+        Button btn_broadcast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_broadcast = findViewById(R.id.btn_broadcast);
+
+        btn_broadcast.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent= new Intent();
+        Bundle bundle =  new Bundle();
+        bundle.putString("activityId", getString(R.string.activity_id));
+        bundle.putString("prsId", getString(R.string.prs_id));
+        bundle.putBoolean("isDataReady", true);
+        intent.setAction("fin.prs.data.readiness");
+        intent.putExtras(bundle);
+        sendBroadcast(intent);
     }
 }
